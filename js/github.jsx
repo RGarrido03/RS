ae12430591c6432614575ed622a91c72bbcd8ab8
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2022-10-23 13:11:31 +0100

    Add one more FontAwesome type

---
+++ js/github.jsx
@@ -0,0 +1,55 @@
+class LatestCommitComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        author: "",
+        branch: "",
+        date: "",
+        sha: "",
+        link: "",
+        error: null
+      };
+    }
+  
+    componentDidMount() {
+      fetch(
+        "https://api.github.com/repos/RGarrido03/RS/branches/main"
+      )
+        .then(response => {
+          response.json().then(json => {
+            console.log(json);
+            if (json.commit) {
+              this.setState({
+                author: json.commit.author.login,
+                branch: json.name,
+                date: json.commit.commit.author.date.split('T',1),
+                sha: json.commit.sha,
+                link: json._links.html
+              });
+            } else {
+              this.setState({
+                error: json.message
+              });
+            }
+          });
+        })
+        .catch(error => {
+          this.setState({
+            error: error
+          });
+          console.log(error);
+        });
+    }
+  
+    render() {
+      const html = this.state.error ? (
+        <div id="error">{this.state.error}</div>
+      ) : (
+        <div id="date">Last commit: {this.state.date}</div>
+      );
+  
+      return <div>{html}</div>;
+    }
+  }
+  
+  ReactDOM.render(<LatestCommitComponent />, document.getElementById("commit"));
